22989be91edf330e9300d9dabcc4d2705cc70dee
LATBauerdick <github@latbauerdick.de>
2019-08-24 23:35:11 -0500

    2019-08-24

--- src/main.rs
+++ src/main.rs
@@ -23,33 +23,13 @@ fn main() {
     println!("x3 is also {:#?}", x3);
 }
 
-
-
-// showMomentum :: HMeas -> String
-// showMomentum h = "pt,pz,fi,E ->" <> (show <<< fromHMeas) h
-
-// fromHMeas :: HMeas -> QMeas -- just drop the d0, z0 part... fix!!!!
-// fromHMeas (HMeas h ch w2pt) = QMeas q cq w2pt where
-//   q = subm 3 h
-//   cq = subm2 3 ch
-
-// -----------------------------------------------
-// -- QMeas
-// -- 3-vector and covariance matrix for momentum measurement
-// --
-// use std::f64::*;
-// data QMeas = QMeas Vec3 Cov3 Number
-// instance Show QMeas where
-//   show = showQMeas
-// -- print QMeas as a 4-momentum vector with errors, use pt and pz
-// showQMeas :: QMeas -> String
 use crate::inp::h_slurp;
 #[test]
 fn test_fvt() {
     let ds = std::fs::read_to_string("dat/tr05129e001412.dat").unwrap();
     let VHMeas {vertex: x, helices: hel} = h_slurp(ds).unwrap();
     for h in &hel { println!("{}", h) };
-    for h in hel { println!("{}", QMeas::from(&h)) };
+    for h in &hel { println!("{}", QMeas::from(h)) };
     println!("initial vertex position -> {}", x);
     let res = String::from("all good?");
     assert!( 1 == 1, "test failed with '{}'", res);
